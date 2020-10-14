480155d04f96deb3cd31367208f77462f14656ad
yw <11>
2020-10-14 17:53:43 +0800

    8

--- dotnet-k3s.groovy
+++ dotnet-k3s.groovy
@@ -88,7 +88,7 @@ pipeline{
             }
             steps{
                 script{
-                    println GIT_Url
+                    println "GIT_Url:" + GIT_Url
                     // 检出GIT上的源代码
                     git.gitcheckout([project_name:"${project_name}",
                                GIT_Branch:"${project_branch}",
