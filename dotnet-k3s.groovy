e26f19a100d5673c36fcd438d229f9f417712727
yw <11>
2020-10-14 18:30:32 +0800

    tre

--- dotnet-k3s.groovy
+++ dotnet-k3s.groovy
@@ -52,7 +52,7 @@ pipeline{
             steps{
                 script{
                     hello()
-                    //Com.read_com_values("${env.WORKSPACE}/jenkinsfile/Values/com_value.yaml")
+                    Com.read_com_values("${env.WORKSPACE}/jenkinsfile/Values/com_value.yaml")
                     //读取项目配置文件
                     Project_values="${env.WORKSPACE}/jenkinsfile/Values/value.yaml"
                     if(fileExists(Com_values) == true){
