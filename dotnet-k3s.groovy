6f5c678e7dd0909c904b4a71f96c46ec440ae9e3
yw <11>
2020-10-15 17:48:38 +0800

    sda

--- dotnet-k3s.groovy
+++ dotnet-k3s.groovy
@@ -133,7 +133,7 @@ pipeline{
                     }else if( env.Deploy_env == 'dev' ){
                         echo "dev docker image build"
                     }
-                    dot.Build_image([project_name="${project_name}",Deploy_env="${Deploy_env}",IMAGE_Name="${IMAGE_Name}"])
+                    dot.ImagesBuild([project_name="${project_name}",Deploy_env="${Deploy_env}",IMAGE_Name="${IMAGE_Name}"])
                 }
             }
         }
