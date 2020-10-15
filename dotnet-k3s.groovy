9c03ad7068b945b70f0c8ddca907f29131417d84
yw <11>
2020-10-15 18:10:30 +0800

    撒大苏打

--- dotnet-k3s.groovy
+++ dotnet-k3s.groovy
@@ -133,7 +133,7 @@ pipeline{
                     }else if( env.Deploy_env == 'dev' ){
                         echo "dev docker image build"
                     }
-                    dot.test([project_name="${project_name}",Deploy_env="${Deploy_env}",IMAGE_Name="${IMAGE_Name}"])
+                    dot.test()
                 }
             }
         }
